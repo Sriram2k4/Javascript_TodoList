617dc0ac6e97d7abef7bfa6a26763faadd2fbf5f
Sriram <sriramparanthaman72@gmail.com>
2022-02-13 21:11:24 +0000

    Started JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click',addTodo);
+
+function addTodo(e){
+    // Prevent Reloading
+    e.preventDefault();
+    const newItem = document.createElement('li');
+
+}

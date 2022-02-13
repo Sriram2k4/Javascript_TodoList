c8cddbc8f24f66116c4567673d1e793989b01cd2
Sriram <sriramparanthaman72@gmail.com>
2022-02-13 21:56:59 +0000

    Basic Functionality

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ todoButton.addEventListener('click',addTodo);
 function addTodo(e){
     // Prevent Reloading
     e.preventDefault();
-    const newItem = document.createElement('li');
+
+    // Creating Div
+    const divTodo = document.createElement('div');
+    divTodo.classList.add('d-flex');
+    divTodo.classList.add('justify-content-center');
+    divTodo.classList.add('mb-2');
+    // Creadting li
+    const liTodo = document.createElement('li');
+    liTodo.classList.add('list-group-item');
+    liTodo.innerHTML = todoInput.value;
+    // Creating Completed Button
+    const completedButton = document.createElement('input');
+    completedButton.classList.add('btn');
+    completedButton.classList.add('btn-success');
+    completedButton.type = 'submit';
+    completedButton.value = 'Completed';
+    // Creating Delete Button
+    const deleteButton = document.createElement('input');
+    deleteButton.classList.add('btn');
+    deleteButton.classList.add('btn-danger');
+    deleteButton.type = 'submit';
+    deleteButton.value = 'Delete';
+    //Appending
+    todoList.appendChild(divTodo);
+    divTodo.appendChild(liTodo);
+    divTodo.appendChild(completedButton);
+    divTodo.appendChild(deleteButton);
 
 }

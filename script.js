e36f2e1589e533d874a963a5ef293195c590a813
Sriram <sriramparanthaman72@gmail.com>
2022-02-14 13:44:59 +0000

    90% Completed

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const todoListItem = document.querySelector('.todo-list div');
 
 todoButton.addEventListener('click',addTodo);
+todoList.addEventListener('click',deleteCheck);
 
 function addTodo(e){
     // Prevent Reloading
@@ -16,23 +18,38 @@ function addTodo(e){
     // Creadting li
     const liTodo = document.createElement('li');
     liTodo.classList.add('list-group-item');
+    liTodo.classList.add('rounded');
+    liTodo.classList.add('w-50');
     liTodo.innerHTML = todoInput.value;
     // Creating Completed Button
-    const completedButton = document.createElement('input');
+    const completedButton = document.createElement('button');
+    completedButton.classList.add('completed-btn');
     completedButton.classList.add('btn');
     completedButton.classList.add('btn-success');
-    completedButton.type = 'submit';
-    completedButton.value = 'Completed';
+    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
     // Creating Delete Button
-    const deleteButton = document.createElement('input');
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-btn');
     deleteButton.classList.add('btn');
     deleteButton.classList.add('btn-danger');
-    deleteButton.type = 'submit';
-    deleteButton.value = 'Delete';
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
     //Appending
     todoList.appendChild(divTodo);
     divTodo.appendChild(liTodo);
+    todoInput.value = "";
     divTodo.appendChild(completedButton);
     divTodo.appendChild(deleteButton);
+}
 
+function deleteCheck(e){
+    const item = e.target;
+    if(item.classList[0] === 'completed-btn'){
+        item.parentElement.classList.toggle('Done');
+    } else if(item.classList[0] === 'delete-btn'){
+        item.parentElement.classList.add('animate__animated');
+        item.parentElement.classList.add('animate__fadeOutDown');
+        setTimeout(function() {
+            item.parentElement.remove();
+        }, 1000);
+    }
 }
